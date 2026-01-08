018c3f614cad240d0a0c7538bb8fc0520d437e4a
denverma7 <denverma7@gmail.com>
2026-01-08 10:30:21 +0500

    Replaced the product list Inages and adjusted footer

--- src/components/whatWeOffer/ProductList.jsx
+++ src/components/whatWeOffer/ProductList.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import ProductList1 from "../../assets/images/productList/ProductList1.png";
+import ProductList2 from "../../assets/images/productList/ProductList2.png";
+import ProductList3 from "../../assets/images/productList/ProductList3.png";
+import ProductList4 from "../../assets/images/productList/ProductList4.png";
 
 const ProductList = () => {
   const products = {
@@ -76,8 +80,9 @@ const ProductList = () => {
             data-aos-duration="1000"
             data-aos-delay="1000"
           >
-            <div className="absolute left-1/2 top-6 w-40 h-40 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#006FB9]" />
-            <div className="absolute left-1/2 top-6 w-[100px] h-[100px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#00C3F3]" />
+            {/* <div className="absolute left-1/2 top-6 w-40 h-40 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#006FB9]" />
+            <div className="absolute left-1/2 top-6 w-[100px] h-[100px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#00C3F3]" /> */}
+            <img src={ProductList1} alt="" className="absolute left-1/2 top-6 -translate-x-1/2 -translate-y-1/2"/>
           </div>
           {/* 1st Column Heading */}
           <h3
@@ -146,10 +151,11 @@ const ProductList = () => {
             data-aos-duration="1000"
             data-aos-delay="1000"
           >
-            <div className="w-60 h-60 rounded-full bg-[#006FB9] flex items-center justify-center mr-3 shrink-0">
-              <div className="w-36 h-36 rounded-full bg-[#00C3F3]"></div>
+            {/* <div className="w-60 h-60 rounded-full bg-[#006FB9] flex items-center justify-center mr-3 shrink-0">
+              <div className="w-36 h-36 rounded-full bg-[#00C3F3]"></div> */}
+              <img src={ProductList2} alt="" className="px-6 "/>
             </div>
-          </div>
+          {/* </div> */}
         </div>
         {/* Third Column - Social*/}
         <div
@@ -159,16 +165,17 @@ const ProductList = () => {
           data-aos-delay="1000"
         >
           <div>
-            {/* Circular element - bottom center */}
+            {/* Circular element - top center */}
             <div
               className="flex items-center justify-center mt-auto overflow-hidden -mb-16"
               data-aos="fade-up"
               data-aos-duration="1000"
               data-aos-delay="1000"
             >
-              <div className="mt-10 w-56 h-56 rounded-full bg-[#006FB9] flex items-center justify-center mr-3 shrink-0">
+              {/* <div className="mt-10 w-56 h-56 rounded-full bg-[#006FB9] flex items-center justify-center mr-3 shrink-0">
                 <div className="w-36 h-36 rounded-full bg-[#00C3F3]"></div>
-              </div>
+              </div> */}
+              <img src={ProductList3} alt="" className="px-6 pt-12 "/>
             </div>
             {/* 3rd Column Heading */}
             <h3 className="uppercase text-[#006FB9] text-2xl mt-30 ml-8 font-bold tracking-wider">
@@ -204,7 +211,7 @@ const ProductList = () => {
         >
           <div>
             {/* 4th Column Heading */}
-            <h3 className="uppercase text-[#006FB9] text-2xl mt-12 md:mt-30 ml-8 font-bold tracking-wider">
+            <h3 className="uppercase text-[#006FB9] text-2xl mt-12 md:mt-20 ml-8 font-bold tracking-wider">
               CSR & <br />
               Sustainability
             </h3>
@@ -233,9 +240,10 @@ const ProductList = () => {
               data-aos-duration="1000"
               data-aos-delay="1000"
             >
-              <div className="mt-10 w-100 h-100 rounded-full bg-[#006FB9] flex items-center justify-center mr-3 shrink-0">
+              {/* <div className="mt-10 w-100 h-100 rounded-full bg-[#006FB9] flex items-center justify-center mr-3 shrink-0">
                 <div className="w-68 h-68 rounded-full bg-[#00C3F3]"></div>
-              </div>
+              </div> */}
+              <img src={ProductList4} alt="" className="pl-4 -pb-8"/>
             </div>
           </div>
         </div>
